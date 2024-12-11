782b9164a758ca9179ea45a88fce21fc948307ae
Nabil Khalid <holanabilkhalid@gmail.com>
2024-12-11 23:28:08 +0800

    feat: update admin route by adding create post & update post status

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -24,6 +24,49 @@ const login = async (req, res) => {
   res.json({ token });
 };
 
+const createPost = async (req, res) => {
+  const { title, content, tag } = req.body;
+
+  try {
+    const post = await prisma.createPost(title, content, tag);
+    return res.status(201).json({
+      success: true,
+      msg: "Post created successfully",
+      post,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to create post",
+      error: err.message,
+    });
+  }
+};
+
+const updatePostStatus = async (req, res) => {
+  let { status } = req.body;
+  let { id } = req.params;
+  status = status === "true" ? true : false;
+  id = Number(id);
+
+  try {
+    const postStatus = await prisma.updatePostStatus(id, status);
+    return res.status(201).json({
+      success: true,
+      msg: `Post ${postStatus.title} status updated succesfully`,
+      postStatus,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: `Failed to update post status`,
+      error: err.message,
+    });
+  }
+};
+
 module.exports = {
   login,
+  createPost,
+  updatePostStatus,
 };
