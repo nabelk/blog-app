7ed7884bf59c7ec51705ac8c4711a6a87fec9e5b
Nabil Khalid <holanabilkhalid@gmail.com>
2024-12-13 23:50:39 +0800

    update: prisma queries

--- api/prisma/queries.js
+++ api/prisma/queries.js
@@ -11,6 +11,55 @@ const prismaQueries = {
       },
     });
   },
+  createPost: async (title, content, tag) => {
+    return await prisma.post.create({
+      data: {
+        title,
+        content,
+        tags: {
+          create: [
+            {
+              tag: {
+                connectOrCreate: {
+                  where: { tag },
+                  create: { tag },
+                },
+              },
+            },
+          ],
+        },
+      },
+    });
+  },
+  updatePostStatus: async (id, published) => {
+    return await prisma.post.update({
+      where: {
+        id,
+      },
+      data: {
+        published,
+      },
+    });
+  },
+  updatePost: async (id, title, content, published) => {
+    return await prisma.post.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        content,
+        published,
+      },
+    });
+  },
+  deletePost: async (id) => {
+    return await prisma.post.delete({
+      where: {
+        id,
+      },
+    });
+  },
 };
 
 module.exports = prismaQueries;
