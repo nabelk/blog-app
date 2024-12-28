85e613944ddae1da072b0b659084ace1683b7ad5
Nabil Khalid <holanabilkhalid@gmail.com>
2024-12-28 22:54:13 +0800

    feat: server

--- api/server.js
+++ api/server.js
@@ -3,11 +3,15 @@ const app = express();
 const passport = require("passport");
 const jwtStrategy = require("./config/passport-jwt.js");
 const routers = require("./routes/routers.js");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 passport.use(jwtStrategy);
 
-app.use("/admin", routers.authRouter);
+app.use("/admin", routers.adminRouter);
+app.use("/api", routers.clientRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
