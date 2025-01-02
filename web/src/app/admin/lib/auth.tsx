c7ecd715a15b5cb48ed314f7dbff0bbdd2a04eba
Nabil Khalid <holanabilkhalid@gmail.com>
2025-01-02 23:58:11 +0800

    feat: create auth context

---
+++ web/src/app/admin/lib/auth.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { createContext, useContext, useState, ReactNode } from 'react';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
+
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+interface AuthContextType {
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+  loading: boolean;
+  error: string | null;
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  const login = async (email: string, password: string) => {
+    try {
+      setLoading(true);
+      setError(null);
+      const response = await axios.post(
+        `${API_URL}/admin/login`,
+        {
+          email,
+          password,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      console.log(response);
+      const { token } = response.data;
+
+      localStorage.setItem('token', token);
+
+      router.push('/admin/dashboard');
+    } catch (err: unknown) {
+      if (axios.isAxiosError(err)) {
+        setError(err.response?.data?.msg);
+      } else {
+        setError('An unknown error occurred');
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    router.push('/admin');
+  };
+
+  return (
+    <AuthContext.Provider value={{ login, logout, loading, error }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within AuthProvider');
+  }
+  return context;
+};
