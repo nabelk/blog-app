7cccd2a3520c78ed355f386971bbeec9549e8b80
Nabil Khalid <holanabilkhalid@gmail.com>
2025-01-14 23:54:11 +0800

    fix: secure admin dashboard

--- web/src/app/admin/dashboard/page.tsx
+++ web/src/app/admin/dashboard/page.tsx
@@ -2,16 +2,25 @@
 import { AdminHome } from './component/admin-home';
 import { Logout } from '../component/logout';
 import { redirect } from 'next/navigation';
+import { jwtDecode } from 'jwt-decode';
+
+interface DecodedToken {
+  exp: number;
+}
 
 export default function AdminPage() {
   const token = localStorage.getItem('token');
-  if (!token) redirect('/admin');
+  const tokenDecoded = token && jwtDecode<DecodedToken>(token);
 
-  if (token)
+  if (!token || (tokenDecoded && tokenDecoded.exp < Date.now() / 1000)) {
+    return redirect('/admin');
+  }
+
+  if (tokenDecoded && tokenDecoded.exp > Date.now() / 1000)
     return (
       <>
         <header className='p-3 px-5 flex justify-between flex-wrap'>
-          <h1>Nabil Khalid</h1>
+          <h1 className='text-lg font-extrabold '>Nabil Khalid</h1>
           <Logout />
         </header>
         <AdminHome />
