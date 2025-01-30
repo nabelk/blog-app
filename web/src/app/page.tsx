285cfbdf0ef1a077c1c5cd43e0a481614f207ecc
Nabil Khalid <holanabilkhalid@gmail.com>
2025-01-30 13:33:42 +0800

    feat: client homepage

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,25 @@
 import axios from 'axios';
 import { Articles } from './component/articles';
+import { Sidebar } from './component/sidebar';
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { tag: string; tagName: string };
+}) {
   const { NEXT_PUBLIC_API_URL } = process.env;
-  const { data, status } = await axios.get(
-    `${NEXT_PUBLIC_API_URL}/api/post/all`
-  );
+  const { tag, tagName } = await searchParams;
+  const { data } = tag
+    ? await axios.get(`${NEXT_PUBLIC_API_URL}/api/post/tag/${tag}`)
+    : await axios.get(`${NEXT_PUBLIC_API_URL}/api/post/all`);
 
-  return <Articles articles={data.posts} />;
+  return (
+    <div className='flex max-[1340px]:flex-col-reverse w-[100vw] justify-center gap-10 flex-wrap'>
+      <Articles
+        articles={tag ? data.post : data.posts}
+        isTag={tag ? tagName : null}
+      />
+      <Sidebar />
+    </div>
+  );
 }
