f2b7725234ab67d7c4958bed8b9588a181e68163
Nabil Khalid <holanabilkhalid@gmail.com>
2025-01-30 13:34:52 +0800

    feat: client global layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Link from 'next/link';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,6 +28,63 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className='border-b-[1px] border-[#99B898] bg-[#d3d877ec] fixed top-0 left-0 w-full'>
+          <div className='text-slate-700 relative flex max-w-screen-xl flex-col overflow-hidden px-4 py-4 md:mx-auto md:flex-row md:items-center'>
+            <Link href='/'>
+              <h3 className='flex cursor-pointer items-center whitespace-nowrap text-2xl font-black'>
+                nk/blg/space
+              </h3>
+            </Link>
+            <input type='checkbox' className='peer hidden' id='navbar-open' />
+            <label
+              className='absolute top-5 right-7 cursor-pointer md:hidden'
+              htmlFor='navbar-open'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                strokeWidth='2'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M4 6h16M4 12h16M4 18h16'
+                />
+              </svg>
+            </label>
+            <div className='peer-checked:mt-8 peer-checked:max-h-56 flex max-h-0 w-full flex-col items-center justify-between overflow-hidden transition-all md:ml-24 md:max-h-full md:flex-row md:items-start'>
+              <ul className='flex flex-col items-center space-y-2 md:ml-auto md:flex-row md:space-y-0'>
+                <li className='font-bold md:mr-12'>
+                  <Link href='/'>Blog</Link>
+                </li>
+
+                <li className='md:mr-12'>
+                  <button className='rounded-full border-2 border-cyan-500 px-2 py-1 text-cyan-600 transition-colors hover:bg-cyan-500 hover:text-white'>
+                    <Link
+                      href='https://github.com/nabelk'
+                      title='Check my github profile'
+                    >
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        x='0px'
+                        y='0px'
+                        width='20'
+                        height='20'
+                        viewBox='0 0 64 64'
+                        className='hover:fill-white'
+                      >
+                        <path d='M32 6C17.641 6 6 17.641 6 32c0 12.277 8.512 22.56 19.955 25.286-.592-.141-1.179-.299-1.755-.479V50.85c0 0-.975.325-2.275.325-3.637 0-5.148-3.245-5.525-4.875-.229-.993-.827-1.934-1.469-2.509-.767-.684-1.126-.686-1.131-.92-.01-.491.658-.471.975-.471 1.625 0 2.857 1.729 3.429 2.623 1.417 2.207 2.938 2.577 3.721 2.577.975 0 1.817-.146 2.397-.426.268-1.888 1.108-3.57 2.478-4.774-6.097-1.219-10.4-4.716-10.4-10.4 0-2.928 1.175-5.619 3.133-7.792C19.333 23.641 19 22.494 19 20.625c0-1.235.086-2.751.65-4.225 0 0 3.708.026 7.205 3.338C28.469 19.268 30.196 19 32 19s3.531.268 5.145.738c3.497-3.312 7.205-3.338 7.205-3.338.567 1.474.65 2.99.65 4.225 0 2.015-.268 3.19-.432 3.697C46.466 26.475 47.6 29.124 47.6 32c0 5.684-4.303 9.181-10.4 10.4 1.628 1.43 2.6 3.513 2.6 5.85v8.557c-.576.181-1.162.338-1.755.479C49.488 54.56 58 44.277 58 32 58 17.641 46.359 6 32 6zM33.813 57.93C33.214 57.972 32.61 58 32 58 32.61 58 33.213 57.971 33.813 57.93zM37.786 57.346c-1.164.265-2.357.451-3.575.554C35.429 57.797 36.622 57.61 37.786 57.346zM32 58c-.61 0-1.214-.028-1.813-.07C30.787 57.971 31.39 58 32 58zM29.788 57.9c-1.217-.103-2.411-.289-3.574-.554C27.378 57.61 28.571 57.797 29.788 57.9z'></path>
+                      </svg>
+                    </Link>
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </header>
         {children}
       </body>
     </html>
