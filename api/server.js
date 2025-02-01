e526343935049b26bf3089e904cfb2509f0f708f
Nabil Khalid <holanabilkhalid@gmail.com>
2025-02-01 22:25:09 +0800

    feat: secure server

--- api/server.js
+++ api/server.js
@@ -4,8 +4,25 @@ const passport = require("passport");
 const jwtStrategy = require("./config/passport-jwt.js");
 const routers = require("./routes/routers.js");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
-app.use(cors());
+app.use(helmet());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests, please try again later.",
+  })
+);
+app.use(
+  cors({
+    origin: [process.env.URL],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 passport.use(jwtStrategy);
