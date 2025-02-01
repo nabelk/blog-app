e917880ad1e5880fcff7e38e0bc8b533bc3d0357
Nabil Khalid <holanabilkhalid@gmail.com>
2025-02-02 03:19:07 +0800

    fix: local storage pre render issue

--- web/src/app/admin/dashboard/page.tsx
+++ web/src/app/admin/dashboard/page.tsx
@@ -1,29 +1,47 @@
 'use client';
 import { AdminHome } from './component/admin-home';
 import { Logout } from '../component/logout';
-import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { jwtDecode } from 'jwt-decode';
+import { useState, useEffect } from 'react';
 
 interface DecodedToken {
   exp: number;
 }
 
 export default function AdminPage() {
-  const token = localStorage.getItem('token');
-  const tokenDecoded = token && jwtDecode<DecodedToken>(token);
+  const [token, setToken] = useState<string | null>(null);
+  const [tokenDecoded, setTokenDecoded] = useState<DecodedToken | null>(null);
+  const router = useRouter();
 
-  if (!token || (tokenDecoded && tokenDecoded.exp < Date.now() / 1000)) {
-    return redirect('/admin');
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const storedToken = localStorage.getItem('token');
+      setToken(storedToken);
+
+      if (storedToken) {
+        const decoded = jwtDecode<DecodedToken>(storedToken);
+        setTokenDecoded(decoded);
+
+        if (decoded.exp < Date.now() / 1000) {
+          router.push('/admin');
+        }
+      } else {
+        router.push('/admin');
+      }
+    }
+  }, [router]);
+
+  if (!token || !tokenDecoded) {
+    return null;
   }
-
-  if (tokenDecoded && tokenDecoded.exp > Date.now() / 1000)
-    return (
-      <>
-        <header className='w-full p-3 pt-20 px-5 flex max-w-screen-xl overflow-hidden  md:mx-auto md:flex-row md:items-center justify-between'>
-          <h1 className='text-lg font-extrabold '>Nabil Khalid</h1>
-          <Logout />
-        </header>
-        <AdminHome />
-      </>
-    );
+  return (
+    <>
+      <header className='w-full p-3 pt-20 px-5 flex max-w-screen-xl overflow-hidden  md:mx-auto md:flex-row md:items-center justify-between'>
+        <h1 className='text-lg font-extrabold '>Nabil Khalid</h1>
+        <Logout />
+      </header>
+      <AdminHome />
+    </>
+  );
 }
--- web/src/app/admin/page.tsx
+++ web/src/app/admin/page.tsx
@@ -5,11 +5,11 @@ import { redirect } from 'next/navigation';
 import { jwtDecode } from 'jwt-decode';
 
 export default function Admin() {
-  const token = localStorage.getItem('token');
-
-  if (token) {
-    const tokenDecoded = jwtDecode(token);
-    if (tokenDecoded.exp! > Date.now() / 1000) redirect('/admin/dashboard');
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token');
+    const tokenDecoded = token && jwtDecode(token);
+    if (tokenDecoded && tokenDecoded.exp! > Date.now() / 1000)
+      redirect('/admin/dashboard');
   }
 
   return (
